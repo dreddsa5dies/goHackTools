ac933c3ad3d61301ab5e58998700ca5dacf371e8
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2020-02-24 17:35:13 +0300

    bad checksum icmp packet

--- 63_icmpTun/client/client.go
+++ 63_icmpTun/client/client.go
@@ -4,38 +4,59 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"os"
 )
 
 func main() {
-	serverAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:12000")
-	if err != nil {
-		log.Fatal(err)
+	if len(os.Args) != 2 {
+		fmt.Println("Использование:	", os.Args[0], "host")
+		os.Exit(1)
 	}
 
-	localAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+	addr, err := net.ResolveIPAddr("ip", os.Args[1])
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка ResolveIPAddr %v", err)
 	}
 
-	conn, err := net.DialUDP("udp", localAddr, serverAddr)
+	conn, err := net.DialIP("ip4:icmp", nil, addr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка DialIP %v", err)
 	}
-	defer conn.Close()
 
-	for {
-		fmt.Print("Input lowercase sentence > ")
-		var msg string
-		_, err := fmt.Scanf("%s", &msg)
-		if err != nil {
-			log.Fatal(err)
-		}
-		buf := []byte(msg)
-		_, err = conn.Write(buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		time.Sleep(time.Second * 1)
+	var msg [512]byte
+	msg[0] = 8  //	echo
+	msg[1] = 0  //	code 0
+	msg[2] = 0  //	checksum, fix later
+	msg[3] = 0  //	checksum, fix later
+	msg[4] = 0  //	identifier[0]
+	msg[5] = 13 //	identifier[1]
+	msg[6] = 0  //	sequence[0]
+	msg[7] = 37 //	sequence[1]
+	len := 8
+
+	// bad checkSum
+	check := checkSum(msg[0:len])
+
+	msg[2] = byte(check >> 8)
+	msg[3] = byte(check & 255)
+
+	_, err = conn.Write(msg[0:len])
+	if err != nil {
+		log.Fatalf("Ошибка conn.Write %v", err)
 	}
+
+	os.Exit(0)
+}
+
+func checkSum(msg []byte) uint16 {
+	sum := 0
+
+	for n := 1; n < len(msg)-1; n += 2 {
+		sum += int(msg[n])*256 + int(msg[n+1])
+	}
+
+	sum = (sum >> 16) + (sum & 0xffff)
+	sum += (sum >> 16)
+	answer := uint16(^sum)
+	return answer
 }
