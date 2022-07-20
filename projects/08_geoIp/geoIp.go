b280b9a7e96606aca3f06a9fdbb99f3c3caf95e9
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-07-20 14:30:45 +0300

    refactoring geoIp

--- projects/08_geoIp/geoIp.go
+++ projects/08_geoIp/geoIp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -10,26 +11,32 @@ import (
 )
 
 func main() {
-	printRecord("173.255.226.98")
+	tmp := []string{
+		"173.255.226.98",
+		"81.2.69.142",
+		"35.184.160.12",
+	}
 
-	printRecord("81.2.69.142")
-
-	printRecord("35.184.160.12")
+	for i := range tmp {
+		if err := printRecord(tmp[i]); err != nil {
+			log.Println(err)
+		}
+	}
 }
 
-func printRecord(tgt string) {
+func printRecord(tgt string) error {
 	if tgt == "" {
-		log.Fatalln("error IP")
+		return errors.New("error IP")
 	}
 
 	absPath, err := filepath.Abs("GeoLite2-City.mmdb")
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 
 	db, err := geoip2.Open(absPath)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer db.Close()
 
@@ -37,14 +44,14 @@ func printRecord(tgt string) {
 
 	record, err := db.City(ip)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
-	defer db.Close()
 
 	fmt.Printf("[*] Target: %v Geo-located.\n", tgt)
 	fmt.Printf("[+] %v, %v, %v\n", record.City.Names["ru"], record.Subdivisions[0].Names["ru"], record.Country.Names["ru"])
 	fmt.Printf("[+] ISO country code: %v\n", record.Country.IsoCode)
 	fmt.Printf("[+] Time zone: %v\n", record.Location.TimeZone)
 	fmt.Printf("[+] Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
+
+	return nil
 }
