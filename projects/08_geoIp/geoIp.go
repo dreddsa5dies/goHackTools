dbd40bae45980600aec8757e551ac2825d19f13a
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-07-20 09:41:30 +0300

    refactoring

--- projects/08_geoIp/geoIp.go
+++ projects/08_geoIp/geoIp.go
@@ -19,10 +19,14 @@ func main() {
 
 func printRecord(tgt string) {
 	if tgt == "" {
-		log.Fatalln("Error IP")
+		log.Fatalln("error IP")
 	}
 
-	absPath, _ := filepath.Abs("GeoLite2-City.mmdb")
+	absPath, err := filepath.Abs("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	db, err := geoip2.Open(absPath)
 	if err != nil {
 		log.Fatal(err)
@@ -30,14 +34,17 @@ func printRecord(tgt string) {
 	defer db.Close()
 
 	ip := net.ParseIP(tgt)
+
 	record, err := db.City(ip)
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
+		return
 	}
+	defer db.Close()
+
 	fmt.Printf("[*] Target: %v Geo-located.\n", tgt)
 	fmt.Printf("[+] %v, %v, %v\n", record.City.Names["ru"], record.Subdivisions[0].Names["ru"], record.Country.Names["ru"])
 	fmt.Printf("[+] ISO country code: %v\n", record.Country.IsoCode)
 	fmt.Printf("[+] Time zone: %v\n", record.Location.TimeZone)
 	fmt.Printf("[+] Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
-	println()
 }
