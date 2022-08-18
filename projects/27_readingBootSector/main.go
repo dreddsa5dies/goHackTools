76a68224d25a2dc8970896ceff9c0d874fd538d2
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-08-18 08:28:17 +0300

    refactoring

--- projects/27_readingBootSector/main.go
+++ projects/27_readingBootSector/main.go
@@ -1,6 +1,6 @@
 package main
 
-// If you need run this programm, you need build this and run from root or sudo users:
+// If you need run this program, you need build this and run from root or sudo users:
 // go build main.go
 // sudo ./main
 
@@ -16,20 +16,26 @@ import (
 func main() {
 	path := "/dev/sda"
 	log.Println("[+] Reading boot sector of " + path)
+
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatal("Error: " + err.Error())
+		log.Println(err)
+		return
 	}
+	defer file.Close()
 
 	// The file.Read() function will read a tiny file in to a large
 	// byte slice, but io.ReadFull() will return an
 	// error if the file is smaller than the byte slice.
 	byteSlice := make([]byte, 512)
+
 	// ReadFull Will error if 512 bytes not available to read
 	numBytesRead, err := io.ReadFull(file, byteSlice)
 	if err != nil {
-		log.Fatal("Error reading 512 bytes from file. " + err.Error())
+		log.Println("error reading 512 bytes from file. " + err.Error())
+		return
 	}
+
 	log.Printf("Bytes read: %d\n\n", numBytesRead)
 	log.Printf("Data as decimal:\n%d\n\n", byteSlice)
 	log.Printf("Data as hex:\n%x\n\n", byteSlice)
