f520cc438cf22060d570c1967980cbaf3f4d0f42
dreddsa5dies <viktor.vladimirovich.solovev@gmail.com>
2022-09-01 08:46:19 +0300

    refactoring

--- projects/49_buildPNGmap/main.go
+++ projects/49_buildPNGmap/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"image/color"
+	"log"
 
-	"github.com/flopp/go-staticmaps"
+	sm "github.com/flopp/go-staticmaps"
 	"github.com/fogleman/gg"
 	"github.com/golang/geo/s2"
 )
@@ -16,12 +17,13 @@ func main() {
 	ctx.AddMarker(sm.NewMarker(s2.LatLngFromDegrees(55.757926, 37.607242), color.RGBA{0xff, 0, 0, 0xff}, 16.0))
 	// and Home
 	ctx.AddMarker(sm.NewMarker(s2.LatLngFromDegrees(55.919609, 37.742699), color.RGBA{0xff, 0, 0, 0xff}, 10.0))
+
 	img, err := ctx.Render()
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
 	if err := gg.SavePNG("my-map.png", img); err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 }
